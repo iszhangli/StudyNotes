45ad1363ed945b072ef79f357a5956d87abce247
zhangli <iszhangli@126.com>
2021-10-10 19:57:22 +0800

    20211010

--- leetcode.sql
+++ leetcode.sql
@@ -1,3 +1,6 @@
+-- 1. 薪水第n高
+-- 2. 连续出现的数字
+
 -- 176.第二高的薪水 由于要返回null 所以需要虚表-----------------------------------------------
 CREATE TABLE IF NOT EXISTS Employee (Id INT, Salary INT);
 TRUNCATE TABLE Employee;
@@ -70,6 +73,7 @@ FROM
 WHERE t.rn < 4;
 
 -- 180. 连续出现的数字
+-- [row_number() order by id / partition by num order by id 相减数值相同]
 CREATE TABLE IF NOT EXISTS LOGS (Id INT, Num INT);
 TRUNCATE TABLE LOGS;
 INSERT INTO LOGS (Id, Num) VALUES ('1', '1');
@@ -79,5 +83,54 @@ INSERT INTO LOGS (Id, Num) VALUES ('4', '2');
 INSERT INTO LOGS (Id, Num) VALUES ('5', '1');
 INSERT INTO LOGS (Id, Num) VALUES ('6', '2');
 INSERT INTO LOGS (Id, Num) VALUES ('7', '2');
+INSERT INTO LOGS (Id, Num) VALUES ('8', '1');
+INSERT INTO LOGS (Id, Num) VALUES ('9', '1');
+INSERT INTO LOGS (Id, Num) VALUES ('10', '1');
+SELECT
+  DISTINCT(T.NUM) AS CONSECUTIVEnUMS
+FROM
+  (SELECT
+    ID,
+    NUM,
+    row_number () over (ORDER BY id) - ROW_NUMBER () OVER (PARTITION BY NUM ORDER BY ID) AS CC
+  FROM
+    LOGS) T
+GROUP BY T.NUM,
+  T.CC
+HAVING COUNT(1) >= 3;
+
+-- 184. 部门工资最高的员工
+-- [最大用group by max]
+DROP TABLE IF EXISTS Employee;
+CREATE TABLE IF NOT EXISTS Employee (Id INT, NAME VARCHAR(255), Salary INT, DepartmentId INT);
+DROP TABLE IF EXISTS Department;
+CREATE TABLE IF NOT EXISTS Department (Id INT, NAME VARCHAR(255));
+TRUNCATE TABLE Employee;
+INSERT INTO Employee (Id, NAME, Salary, DepartmentId) VALUES ('1', 'Joe', '70000', '1');
+INSERT INTO Employee (Id, NAME, Salary, DepartmentId) VALUES ('2', 'Jim', '90000', '1');
+INSERT INTO Employee (Id, NAME, Salary, DepartmentId) VALUES ('3', 'Henry', '80000', '2');
+INSERT INTO Employee (Id, NAME, Salary, DepartmentId) VALUES ('4', 'Sam', '60000', '2');
+INSERT INTO Employee (Id, NAME, Salary, DepartmentId) VALUES ('5', 'Max', '90000', '1');
+TRUNCATE TABLE Department;
+INSERT INTO Department (Id, NAME) VALUES ('1', 'IT');
+INSERT INTO Department (Id, NAME) VALUES ('2', 'Sales');
+
+SELECT
+  T2.Name AS Department1,
+  T1.Name AS Employee1,
+  Salary
+FROM
+  Employee AS T1
+  LEFT JOIN Department AS T2
+    ON T1.DepartmentId = T2.Id
+WHERE (T1.DepartmentId, T1.Salary) IN
+  (SELECT
+    DepartmentId,
+    MAX(Salary)
+  FROM
+    Employee
+  GROUP BY DepartmentId);
+
+
 
 
