52e4f5e2bf664c1c9cd904fb0d17612ae9b8834c
zhangli <iszhangli@126.com>
2021-11-02 21:00:42 +0800

    20211102

--- leetcode/575.分糖果.py
+++ leetcode/575.分糖果.py
@@ -8,17 +8,23 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         # hash sorted
-        d = {}
-        for i in candyType:
-            if i is d.keys():
-                d[i] += 1
-            else: d[i] = 1
-        d = sorted(d.items(), key=lambda x: x[1])
-        re = 0
-        for k, v in d:
-            re += 1
-            if re >= len(candyType) / 2:
-                return re
-        return re
+        # d = {}
+        # for i in candyType:
+        #     if i is d.keys():
+        #         d[i] += 1
+        #     else: d[i] = 1
+        # d = sorted(d.items(), key=lambda x: x[1])
+        # re = 0
+        # for k, v in d:
+        #     re += 1
+        #     if re >= len(candyType) / 2:
+        #         return re
+        # return re
+        s = set(candyType)
+        l = candyType
+        if len(s) < len(l) / 2: 
+            return len(s)
+        else: 
+            return int(len(l) / 2)
 # @lc code=end
 
