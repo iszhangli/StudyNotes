e4843e5c25116c503f9e11021c599baa17e03791
zhangli <iszhangli@126.com>
2022-09-29 14:24:40 +0800

    20220929

--- leetcode/33.搜索旋转排序数组.py
+++ leetcode/33.搜索旋转排序数组.py
@@ -6,6 +6,27 @@
 
 # @lc code=start
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums, target: int) -> int:
+        low, high = 0, len(nums)-1
+        while(low<=high):
+            mid = low + (high-low)//2
+            if target == nums[mid]:
+                return mid
+            
+            if nums[low] <= nums[mid]: # [1, 2, 3, 4, 0]
+                if nums[low] <= target < nums[mid]:
+                    high = mid-1
+                else:
+                    low = mid+1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid -1
+        return -1 
+
+nums = [3,1]
+target = 1           
+print(Solution().search(nums, target))
 # @lc code=end
 
