08a326ecd330994a3643fec11531de9f52036199
zhangli <iszhangli@126.com>
2022-10-08 17:18:48 +0800

    20221008

--- leetcode/55.跳跃游戏.py
+++ leetcode/55.跳跃游戏.py
@@ -5,7 +5,23 @@
 #
 
 # @lc code=start
+from ast import List
+
+
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
+    def canJump(self, nums):
+        length, right = len(nums) - 1, len(nums) - 1 
+        while right >= 0:
+            ix = 1
+            while nums[right] == 0 and right < length:
+                if ix > right:
+                    return False
+                if nums[right-ix] > ix:
+                    break
+                ix += 1
+            right -= ix
+        return True
+l = [0,1]
+Solution().canJump(l)
 # @lc code=end
 
